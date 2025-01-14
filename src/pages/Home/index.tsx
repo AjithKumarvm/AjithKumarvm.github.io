9915a2dcb37f38cda3c47757c3f9856cffa562b2
Ajith kumar VM <ajithkumarvm@gmail>
2025-01-14 17:56:29 +0530

    feat: update 4

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,14 @@
 
-import Hyperspeed from './HyperSpeed'
 import NamePhoto from './NamePhoto'
 import styles from './style.module.scss'
+import HyperSpped from './HyperSpeed'
 
 
 const Home = () => {
   return <div className={styles.wrapper}>
     <NamePhoto />
     {/* <VideoBg src="/hero.mp4" /> */}
-    <Hyperspeed />
+    <HyperSpped />
   </div>
 }
 
